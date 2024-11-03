8930a70736af0062cac026455846c475b2fdd1bf
satish7654 <satishsingh202dd+@gmail.com>
2024-11-03 20:16:04 +0545

    Second Update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const app = express();
-require("dotenv").config();
-const PORT = process.env.PORT;
-
+const { PORT } = require("./config");
 app.get("/test", (req, res) => {
   res.json({
     msg: "Test ok",
